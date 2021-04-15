234a903d2e35701a7d514f958ff1c844f96cb9c0
Richard Webb <richard.webb@boldonjames.com>
2021-04-15 23:00:34 +0100

    timespan test

---
+++ SwashbuckleTimespan/TimeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SwashbuckleTimespan.Controllers
+{
+    public class TSResult
+    {
+        public TimeSpan time { get; set; }
+    }
+
+    [ApiController]
+    [Route("[controller]")]
+    public class TimeController : ControllerBase
+    {
+        [HttpGet]
+        public TSResult Get()
+        {
+            return new TSResult();
+        }
+    }
+}
